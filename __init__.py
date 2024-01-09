80f79a55e24f5bff41613dd4800b1cf3dbe60dd9
Topper <25074127+MrAdamBlack@users.noreply.github.com>
2024-01-09 17:10:10 +1100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from  .CheckProgress import *
+
+NODE_CLASS_MAPPINGS = { "CHECK_PROGRESS": CheckProgress }
+NODE_DISPLAY_NAME_MAPPINGS = { "Example": "Check Progress" }
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
